9480d2220820e7a1bf6967430368902865d4d4d7
Fiqi Arifianto <fiqi.afui@gmail.com>
2022-08-12 14:07:35 +0700

    add Camps Migration & Model

---
+++ app/Models/Camps;.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Camps extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'camps';
+
+    protected $fillable = [
+        'title',
+        'price',
+    ];
+}
